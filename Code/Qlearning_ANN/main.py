2d3f89ed778be971ff2b9a8c29a554dd2340aa56
Maximilien <maxkulbi83@gmail.com>
2025-01-19 11:51:24 +0100

    Adding main with agents

--- Code/Qlearning_ANN/main.py
+++ Code/Qlearning_ANN/main.py
@@ -1,31 +1,138 @@
-from IPython.display import clear_output
-import Code.Qlearning_ANN.Qlearning as Qlearning
+import pygame
 
-Qlearning = Qlearning.Qlearning()
+import SnakeGame as sg
+import QLearning as ql
+import UserGame as ug
+import ANNTraining as ann
 
-while(1):
+import csv
 
-    print('Please select what you want to do:\n')
-    print('1- User controlled snake\n')
-    print('2- Qlearning training\n')
-    print('3- Qlearning game visualization\n')
-    print('4- ANN training\n')
-    print('5- ANN game visualization\n')
 
-    action = input('Please select what you want to do:')
+################################### For data collection ###################################
+offset = 66
+def data_collection(file_index):
+    in_path = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/labels'
+    out_path = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/outfiles'
+    offset_path_in = in_path + str(offset+file_index) +'.csv'
+    offset_path_out = out_path + str(offset+file_index) +'.csv'
+    return [offset_path_in, offset_path_out]
 
-    clear_output(wait=True)
+################################### Custom functions to try out our models ###################################
+def play_with_ql_model(file_index):
 
-    if action == '1':
-        pass
-    elif action == '2':
-        Qlearning.QlearningTraining()
-    elif action == '3':
-        Qlearning.startGameQlearning()
-    elif action == '4':
-        pass
-    elif action == '5':
-        pass
-    else:
-        print('Invalid action')
-        continue
+    state = game.reset()
+    state_index = game.get_state_index(state)
+    total_reward = 0
+    done = False
+
+    # Recording objects
+    in_path, out_path = data_collection(file_index)
+    user = ug.UserGame(in_path, out_path)
+    file = open(in_path, 'w', newline='\n')
+    writer = csv.writer(file)
+
+    while not done:
+
+        # Registering Q-learning
+        action = ql_agent.get_best_action(state_index)
+
+        # Record the data if wanted
+        user.save_state(game.snake, game.food) # Saves an environment picture
+        writer.writerow([action / 4])
+
+        # Take the action 
+        state, reward, done = game.step(action)
+        state_index = game.get_state_index(state)
+        total_reward += reward
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        game.render()
+        game.clock.tick(sg.FPS)
+
+
+    print(f"Game Over! Total Score: {game.score}")
+    file.close()
+    user.close_files()
+
+def play_with_ann_model():
+
+    state = game.reset()
+    state_index = game.get_state_index(state)
+    total_reward = 0
+    done = False
+
+    while not done:
+
+        # Infering with ann
+        action = ann_agent.infer_from_state(state)
+
+        # Take the action 
+        state, reward, done = game.step(action)
+        total_reward += reward
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        game.render()
+        game.clock.tick(sg.FPS)
+
+    print(f"Game Over! Total Score: {game.score}")
+
+def play_with_user():
+
+    state = game.reset()
+    total_reward = 0
+    done = False
+
+    in_path, out_path = data_collection(0)
+    user = ug.UserGame(in_path, out_path)
+
+    while not done:
+
+        # Gather user data
+        action = user.capture_user_input()
+        user.save_state(game.snake, game.food)
+
+        # Take the action 
+        state, reward, done = game.step(action)
+        total_reward += reward
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        game.render()
+        game.clock.tick(sg.FPS)
+
+    print(f"Game Over! Total Score: {game.score}")
+    user.close_files()
+
+
+################################### Our objects ###################################
+game = sg.SnakeGame()
+ql_agent = ql.QLearning()
+# ann_agent = ann.ANNTraining()
+
+################################### Filling the Qtable ###################################
+ql_agent.fill_qtable(game)
+
+################################### Setting up ANN agent ###################################
+# duplicate_path_outputs = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/testout.csv'
+# duplicate_path_labels = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/testin.csv'
+
+# ann_agent.convert_ds(duplicate_path_outputs, duplicate_path_labels)
+# ann_agent.train_model(20, 18) # Number of epochs and batch-size
+
+# print("Modeled trained ! ")
+
+
+################################### The game loop ###################################
+for i in range(100): # We can loop if we want to save several games from the qtable
+    game.launch_game()
+    play_with_ql_model(i)
+    pygame.quit()
+
