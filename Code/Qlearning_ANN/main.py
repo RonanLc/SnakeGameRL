5762b9ca1952d2527ada1ad24581dba737f9464f
Maximilien <maxkulbi83@gmail.com>
2025-01-19 17:12:14 +0100

    Bug fix
    Bug involving the ann_agent fixed

--- Code/Qlearning_ANN/main.py
+++ Code/Qlearning_ANN/main.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 
 import SnakeGame as sg
 import QLearning as ql
@@ -9,10 +10,10 @@ import csv
 
 
 ################################### For data collection ###################################
-offset = 66
+offset = 0
 def data_collection(file_index):
-    in_path = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/labels'
-    out_path = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/outfiles'
+    in_path = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/testlabels'
+    out_path = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/testoutfiles'
     offset_path_in = in_path + str(offset+file_index) +'.csv'
     offset_path_out = out_path + str(offset+file_index) +'.csv'
     return [offset_path_in, offset_path_out]
@@ -59,15 +60,18 @@ def play_with_ql_model(file_index):
 
 def play_with_ann_model():
 
-    state = game.reset()
-    state_index = game.get_state_index(state)
     total_reward = 0
     done = False
+
+    in_path, out_path = data_collection(0)
+    user = ug.UserGame(in_path, out_path)
 
     while not done:
 
         # Infering with ann
-        action = ann_agent.infer_from_state(state)
+        env_image = user.save_state(game.snake, game.food).flatten()
+        input_vector = np.expand_dims(env_image, axis=0) # To make the image readable 
+        action = ann_agent.infer_from_state(input_vector)
 
         # Take the action 
         state, reward, done = game.step(action)
@@ -114,25 +118,25 @@ def play_with_user():
 
 ################################### Our objects ###################################
 game = sg.SnakeGame()
-ql_agent = ql.QLearning()
-# ann_agent = ann.ANNTraining()
+# ql_agent = ql.QLearning()
+ann_agent = ann.ANNTraining()
 
 ################################### Filling the Qtable ###################################
-ql_agent.fill_qtable(game)
+# ql_agent.fill_qtable(game)
 
 ################################### Setting up ANN agent ###################################
-# duplicate_path_outputs = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/testout.csv'
-# duplicate_path_labels = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/testin.csv'
+duplicate_path_outputs = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/outfiles.csv'
+duplicate_path_labels = 'C:/Users/kulbi/OneDrive - Université Nice Sophia Antipolis/polytech_lessons/ROBO5/RL/Projet/ANN_datasets/labels.csv'
 
-# ann_agent.convert_ds(duplicate_path_outputs, duplicate_path_labels)
-# ann_agent.train_model(20, 18) # Number of epochs and batch-size
+ann_agent.convert_ds(duplicate_path_outputs, duplicate_path_labels)
+ann_agent.train_model(10, 32) # Number of epochs and batch-size
 
-# print("Modeled trained ! ")
+print("Modeled trained ! ")
 
 
 ################################### The game loop ###################################
-for i in range(100): # We can loop if we want to save several games from the qtable
+for i in range(1): # We can loop if we want to save several games from the qtable
     game.launch_game()
-    play_with_ql_model(i)
+    play_with_ann_model()
     pygame.quit()
 
